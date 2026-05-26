7aa40d6837a61b5ceb15eeeb5c332eeda1d7a551
Iannn-vbeta <232410103085@mail.unej.ac.id>
2026-05-26 19:48:41 +0700

    style : change name entrypoint

--- app.py
+++ app.py
@@ -964,7 +964,7 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "api_nlp_tabular:app",
+        "app:app",
         host="0.0.0.0",
         port=8000,
         reload=True,
